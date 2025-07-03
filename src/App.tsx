426e4af12a0db817a24539e32eca6bebf3e4042e
Jatin Saini <jatinkhushi5555@gmail.com>
2025-07-03 21:18:44 +0530

    Project Initialization and typography and colors configurations

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-background-default text-primary h-screen flex items-center justify-center text-display">
+        Welcome to PeerHub Admin Panel
+      </div>
+    </>
+  )
+}
+
+export default App
